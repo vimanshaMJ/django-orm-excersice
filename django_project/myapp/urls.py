b9e32dda0de765e205d57ffbf35c41d6a1a4fb9b
vimansha <vimanshamadushani88@gmail.com>
2024-07-01 21:01:14 +0530

    feat: half of the endpoints

--- django_project/myapp/urls.py
+++ django_project/myapp/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
-from . import views
+from .views import  AnnotationPostView, BulkCreateUserView, HomeView, PostFirstView, PostGetOrCreateView, PostLastView, PostListView, ProfileListView, UserListView, UserByEmailView, UsersByExcludeUsernameView, UserByIdView, CreateUserView, UserCountView, ProfileUpdateOrCreateView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('users/', views.user_list, name='user_list'),
-    path('users/<str:email>/', views.users_by_email, name='users_by_email'),
-    path('users/exclude/<str:username>/', views.users_by_exclude_username, name='users_by_username'),
-    path('users/<int:id>/', views.users_by_id, name='users_by_id'),
-    
+    path('', HomeView.as_view(), name='home'),
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/email/<str:email>/', UserByEmailView.as_view(), name='user_by_email'),
+    path('users/exclude/<str:username>/', UsersByExcludeUsernameView.as_view(), name='users_by_exclude_username'),
+    path('users/<int:id>/', UserByIdView.as_view(), name='user_by_id'),
+    path('users/create/', CreateUserView.as_view(), name='create_user'),
+    path('users/bulk_create/', BulkCreateUserView.as_view(), name='bulk_create_user'),
+    path('users/count/', UserCountView.as_view(), name='user_count'),
+
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/get_or_create/', PostGetOrCreateView.as_view(), name='post_get_or_create'),
+    path('posts/annotate_comments_count/', AnnotationPostView.as_view(), name='annotate_comments_count'),
+    path('posts/first/', PostFirstView.as_view(), name='post_first'),
+    path('posts/last/', PostLastView.as_view(), name='post_last'),
+
+    path('profile/', ProfileListView.as_view(), name='profile_list'),
+    path('profile/update_or_create/', ProfileUpdateOrCreateView.as_view(), name='profile_update_or_create'),
+
+
+
+   
+
 ]
-
-
--- django_project/myapp/views.py
+++ django_project/myapp/views.py
@@ -1,34 +1,164 @@
 import json
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-from .models import User, Post, Profile, Comment, Tag, PostTag
-from django.db.models import Count, Sum, Avg
-from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from .models import User, Profile, Post, Comment, Tag, PostTag
+from django.db.models import Sum, Avg, Count, Max, Min
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Hey there! This is a Django ORM exercise 01.")
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse("Hey there! This is a Django ORM exercise 01.")
 
 
-def user_list(request):
-    users = User.objects.all()
-    users_list = list(users.values())
-    return JsonResponse(users_list, safe=False)
+# User views
+class UserListView(View):
+    def get(self, request):
+        users = User.objects.all()
+        users_list = list(users.values())
+        return JsonResponse(users_list, safe=False)
 
-def users_by_email(request, email):
-    user = User.objects.filter(email=email)
-    user_list = list(user.values())
-    return JsonResponse(user_list, safe=False)
+class UserByEmailView(View):
+    def get(self, request, email):
+        user = User.objects.filter(email=email)
+        user_list = list(user.values())
+        return JsonResponse(user_list, safe=False)
 
-def users_by_exclude_username(request, username):
-    user = User.objects.exclude(username=username)
-    user_list = list(user.values())
-    return JsonResponse(user_list, safe=False)
+class UsersByExcludeUsernameView(View):
+    def get(self, request, username):
+        user = User.objects.exclude(username=username)
+        user_list = list(user.values())
+        return JsonResponse(user_list, safe=False)
 
-def users_by_id(id):
-    user = User.objects.get(id=id)
-    user_list = list(user.values())
-    return JsonResponse(user_list, safe=False)
+class UserByIdView(View):
+    def get(self, request, id):
+        user = get_object_or_404(User, id=id)
+        user_data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        }
+        return JsonResponse(user_data)
 
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateUserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        user = User.objects.create(
+            username=data['username'],
+            email=data['email'],
+            first_name=data['first_name'],
+            last_name=data['last_name']
+        )
+        user.save()
+        return JsonResponse({"message": "User created successfully!"}, status=201)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class BulkCreateUserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        users = []
+        for user in data:
+            users.append(User(
+                username=user['username'],
+                email=user['email'],
+                first_name=user['first_name'],
+                last_name=user['last_name']
+            ))
+        User.objects.bulk_create(users)
+        return JsonResponse({"message": "Users created successfully!"}, status=201)
+    
+class UserCountView(View):
+    def get(self, request):
+        count = User.objects.all().count()
+        return JsonResponse({"count": count}, status=200)
+    
+
+# Post views
+class PostListView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        posts_list = list(posts.values())
+        return JsonResponse(posts_list, safe=False)
+    
+
+class PostGetOrCreateView(View):
+    def get(self, request):
+        title = request.GET.get('title')
+        author_id = request.GET.get('author_id')
+        author = get_object_or_404(User, id=author_id)
+        post, created = Post.objects.get_or_create(title=title, author=author)
+        post_data = {
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'author_id': post.author.id,
+            'author': post.author.username,
+            'created_at': post.created_at,
+            'updated_at': post.updated_at
+        }
+        return JsonResponse(post_data)
+    
+
+# Profile views
+class ProfileListView(View):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        profiles_list = list(profiles.values())
+        return JsonResponse(profiles_list, safe=False)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class ProfileUpdateOrCreateView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        user = get_object_or_404(User, id=data['user_id'])
+        profile, created = Profile.objects.update_or_create(
+            user=user,
+            defaults={
+                'user_id': data['user_id'],
+                'bio': data['bio'],
+                'birth_date': data['birth_date']
+            }
+        )
+        return JsonResponse({"message": "Profile updated/created successfully!"}, status=200)
+    
+class AnnotationPostView(View):
+    def get(self, request):
+        posts = Post.objects.annotate(Count('comment'))
+        posts_list = list(posts.values())
+        return JsonResponse(posts_list, safe=False)
+    
+class PostFirstView(View):
+    def get(self, request):
+        post = Post.objects.first()
+        post_data = {
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'author_id': post.author.id,
+            'author': post.author.username,
+            'created_at': post.created_at,
+            'updated_at': post.updated_at
+        }
+        return JsonResponse(post_data)
+
+class PostLastView(View):
+    def get(self, request):
+        post = Post.objects.last()
+        post_data = {
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'author_id': post.author.id,
+            'author': post.author.username,
+            'created_at': post.created_at,
+            'updated_at': post.updated_at
+        }
+        return JsonResponse(post_data)
+    
+    
