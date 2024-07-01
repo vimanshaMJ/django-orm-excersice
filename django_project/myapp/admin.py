dcdbb0bfef316964c1ecd0314e64c29a7429a2a1
vimansha <vimanshamadushani88@gmail.com>
2024-07-01 10:44:43 +0530

    feat: register models & set urls

--- django_project/myapp/admin.py
+++ django_project/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Profile, Post, Comment, Tag, PostTag
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(PostTag)
